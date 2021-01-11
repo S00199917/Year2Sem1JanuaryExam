38d9b5e18b330ec86e78e3fa48d40cc060f6bef3
Jack <jackpmalone01@gmail.comgit config --global user.name jackMalone2001>
2021-01-11 10:45:05 +0000

    New accounts will be displayed to the screen and can be filltered by different account types

--- Year2Sem1JanuaryExam/MainWindow.xaml.cs
+++ Year2Sem1JanuaryExam/MainWindow.xaml.cs
@@ -22,16 +22,67 @@ namespace Year2Sem1JanuaryExam
     public partial class MainWindow : Window
     {
         static ObservableCollection<Account> accounts = new ObservableCollection<Account>();
+        static ObservableCollection<CurrentAccount> currents = new ObservableCollection<CurrentAccount>();
+        static ObservableCollection<SavingsAccount> savings = new ObservableCollection<SavingsAccount>();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            SavingsAccount savings = new SavingsAccount("BruceBat", "WayneCave", 15000m, 22);
+            SavingsAccount savings0 = new SavingsAccount("Cave", "Johnson", 15000m, 22);
+            SavingsAccount savings1 = new SavingsAccount("Frank", "Conway", 11254m, 113);
 
-            accounts.Add(savings);
+            savings.Add(savings0);
+            savings.Add(savings1);
+
+            CurrentAccount current = new CurrentAccount("Jimmy", "Money", 5512m, 42);
+            CurrentAccount current1 = new CurrentAccount("John", "Doe", 552132m, 551);
+
+            currents.Add(current);
+            currents.Add(current1);
+
+            accounts.Add(savings0);
+            accounts.Add(savings1);
+            accounts.Add(current);
+            accounts.Add(current1);
 
             lstbxAccounts.ItemsSource = accounts;
+        }
+
+        private void chbxSavingsAccounts_Click(object sender, RoutedEventArgs e)
+        {
+            lstbxAccounts.ItemsSource = null;
+
+            if (chbxCurrentAccounts.IsChecked == false && chbxSavingsAccounts.IsChecked == true)
+            {
+                lstbxAccounts.ItemsSource = savings;
+            }
+            else if (chbxCurrentAccounts.IsChecked == true && chbxSavingsAccounts.IsChecked == false)
+            {
+                lstbxAccounts.ItemsSource = currents;
+            }
+            else
+            {
+                lstbxAccounts.ItemsSource = accounts;
+            }
+        }
+
+        private void chbxCurrentAccounts_Click(object sender, RoutedEventArgs e)
+        {
+            lstbxAccounts.ItemsSource = null;
+
+            if (chbxSavingsAccounts.IsChecked == false && chbxCurrentAccounts.IsChecked == true)
+            {
+                lstbxAccounts.ItemsSource = currents;
+            }
+            else if (chbxSavingsAccounts.IsChecked == true && chbxCurrentAccounts.IsChecked == false)
+            {
+                lstbxAccounts.ItemsSource = savings;
+            }
+            else
+            {
+                lstbxAccounts.ItemsSource = accounts;
+            }
         }
     }
 }
